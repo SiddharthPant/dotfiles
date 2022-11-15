dfeeee8ed818a1712a6f8e2b7243516185a6f0ac
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2022-11-15 07:06:43 +0530

    Add i3 configs and update zsh and neovim configs to latest

--- .zshrc
+++ .zshrc
@@ -6,20 +6,31 @@ antigen init ~/.antigenrc
 alias vim=nvim
 export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git'"
 export EDITOR="nvim"
+#
+# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+# PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"
+# PROMPT+=' %{$fg[cyan]%}%3~%{$reset_color%} $(git_prompt_info)'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/sid/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/sid/mambaforge/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/sid/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/sid/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/home/sid/mambaforge/etc/profile.d/conda.sh" ]; then
+        . "/home/sid/mambaforge/etc/profile.d/conda.sh"
     else
-        export PATH="/home/sid/anaconda3/bin:$PATH"
+        export PATH="/home/sid/mambaforge/bin:$PATH"
     fi
 fi
 unset __conda_setup
+
+if [ -f "/home/sid/mambaforge/etc/profile.d/mamba.sh" ]; then
+    . "/home/sid/mambaforge/etc/profile.d/mamba.sh"
+fi
 # <<< conda initialize <<<
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,140 @@
+require('plugins')
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = { 'sumneko_lua', 'pyright', 'tsserver' }
+})
+
+local vim = vim
+local o = vim.opt
+local g = vim.g
+local bo = vim.bo
+
+o.nu = true
+o.relativenumber = true
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+bo.swapfile = false
+o.backup = false
+o.completeopt='menuone,noinsert,noselect'
+o.smartindent = true
+
+o.wrap = false
+g.mapleader = ' '
+o.termguicolors = true
+
+
+local key_mapper = function(mode, key, result)
+  vim.api.nvim_set_keymap(
+    mode,
+    key,
+    result,
+    {noremap = true, silent = true}
+  )
+end
+
+key_mapper('i', 'jk', '<ESC>')
+key_mapper('i', 'JK', '<ESC>')
+key_mapper('i', 'jK', '<ESC>')
+key_mapper('v', 'jk', '<ESC>')
+key_mapper('v', 'JK', '<ESC>')
+key_mapper('v', 'jK', '<ESC>')
+
+-- require('tokyonight').setup({
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+--   style = 'storm',
+--   transparent = false, -- Enable this to disable setting the background color
+--   styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    -- Background styles. Can be 'dark', 'transparent' or 'normal'
+--     sidebars = 'normal', -- style for sidebars, see below
+    -- floats = 'dark', -- style for floating windows
+--   },
+-- })
+
+vim.cmd('colorscheme tokyonight')
+
+require('lualine').setup()
+
+-- require'nvim-web-devicons'.setup {
+ -- your personnal icons can go here (to override)
+ -- you can specify color or cterm_color instead of specifying both of them
+ -- DevIcon will be appended to `name`
+ -- globally enable different highlight colors per icon (default to true)
+ -- if set to false all icons will have the default icon's color
+--  color_icons = true;
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+--  default = true;
+-- }
+
+local configs = require'nvim-treesitter.configs'
+configs.setup {
+  ensure_installed = {'python', 'typescript'},
+  highlight = {
+    enable = true,
+  }
+}
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+require('lspconfig')['pyright'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig')['tsserver'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+require('lspconfig')['sumneko_lua'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- vim.cmd( 'set spell' )
