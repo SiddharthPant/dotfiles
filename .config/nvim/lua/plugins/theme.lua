a16e40adc36bf8a1c5c86f5ceaaf39693ae3b4f0
SiddharthPant <spsiddharthpant@gmail.com>
2026-04-26 13:57:03 +0530

    Use GitHub light theme across tools

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,5 +1,25 @@
-require("catppuccin").setup({
-	transparent_background = true,
+require("github-theme").setup({
+	options = {
+		transparent = true,
+		styles = {
+			comments = "italic",
+			conditionals = "italic",
+			constants = "italic",
+			keywords = "italic",
+			types = "italic",
+		},
+	},
+	groups = {
+		all = {
+			FloatBorder = { fg = "#8c959f" },
+			WinSeparator = { fg = "#8c959f" },
+			SnacksPickerBorder = { fg = "#8c959f" },
+			SnacksPickerInputBorder = { fg = "#8c959f" },
+			SnacksPickerListBorder = { fg = "#8c959f" },
+			SnacksPickerPreviewBorder = { fg = "#8c959f" },
+			SnacksNotifierBorder = { fg = "#8c959f" },
+		},
+	},
 })
 
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("github_light")
