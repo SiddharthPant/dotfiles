1ef709716319bcf0e4f7357e5eb06d3220e7b894
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-08-22 02:28:26 +0530

    Revert back editor changes

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,4 +1,6 @@
 ;; Reset GC after startup (early-init.el cranked it to the moon).
 (setq gc-cons-threshold (* 32 1024 1024))
 (load-theme 'modus-vivendi t)
-(set-face-attribute 'default nil :height 140)
+(set-face-attribute 'default nil
+		    :family "Iosevka Nerd Font Mono"
+		    :height 140)
