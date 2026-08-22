a3c31b3074e1da7b2fe72566c37c5aa27a5b0ee9
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-08-23 01:29:59 +0530

    emacs: Simplify config and cleanup

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,11 +1,10 @@
 ;;; early-init.el --- Runs before the GUI frame is created -*- lexical-binding: t; -*-
 
-;; Crank the GC threshold during startup; init.el resets it to a sane value.
+;; Defer garbage collection during startup; init.el restores it.
 (setq gc-cons-threshold most-positive-fixnum)
 
 (setq select-enable-clipboard t)
 
-;; Kill the UI chrome before the first frame paints (avoids a visible flash).
 (setq inhibit-startup-message t
       frame-resize-pixelwise t
       initial-frame-alist '((fullscreen . maximized))
@@ -18,7 +17,6 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
-;; Native-comp is great but noisy on first compile; keep warnings quiet.
 (setq native-comp-async-report-warnings-errors 'silent)
 
-;;; early-init.el ends heres
+;;; early-init.el ends here
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,51 +1,42 @@
-;; Reset GC after startup (early-init.el cranked it to the moon).
-(setq gc-cons-threshold (* 32 1024 1024))
+;;; init.el --- Personal Emacs configuration -*- lexical-binding: t; -*-
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (load custom-file 'noerror)
 
-(load-theme 'modus-vivendi t)
+;; Packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Appearance
+(use-package naysayer-theme
+  :ensure t
+  :config
+  (load-theme 'naysayer t))
 (set-face-attribute 'default nil
-			    :family "Iosevka Term"
+		    :family "Iosevka Term"
 		    :height 140)
 
 ;; Editing
-(column-number-mode 1)
-(show-paren-mode 1)
-
+(setq make-backup-files nil
+      display-line-numbers-type 'relative)
 (setq-default indent-tabs-mode nil
               tab-width 4)
 
-(setq display-line-numbers-type 'relative)
+(column-number-mode 1)
+(show-paren-mode 1)
+(fido-vertical-mode 1)
 
-(defun sid/programming-setup ()
-  (display-line-numbers-mode 1)
-  (add-hook 'before-save-hook
-            #'delete-trailing-whitespace nil t))
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
 
-(add-hook 'prog-mode-hook #'sid/programming-setup)
-
-;; Org
-(setq org-directory "~/Documents/org"
-      org-agenda-files (list org-directory)
-      org-default-notes-file (expand-file-name "inbox.org" org-directory)
-      org-capture-templates
-      `(("t" "Task" entry
-         (file ,org-default-notes-file)
-         "* TODO %?\n  %U\n")))
-
-(keymap-global-set "C-c a" #'org-agenda)
-(keymap-global-set "C-c c" #'org-capture)
-
-;; Rust
-(require 'rust-ts-mode)
-(add-hook 'rust-ts-mode-hook #'eglot-ensure)
-
-;; Version control
-(use-package magit
-  :ensure t
-  :bind (("C-x g" . magit-status)))
-
+;; Development
+(use-package rust-mode :ensure t)
+(use-package magit :ensure t)
 (use-package majutsu
   :vc (:url "https://github.com/0WD0/majutsu")
   :commands (majutsu majutsu-log))
+
+;; Restore garbage collection after startup.
+(setq gc-cons-threshold (* 32 1024 1024))
+
+;;; init.el ends here
