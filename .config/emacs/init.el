2263f58b6321af19af0f4e0cabdddcfdc864d26d
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-08-23 02:05:33 +0530

    emacs: refine theme and editing workflow

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -9,17 +9,20 @@
 (package-initialize)
 
 ;; Appearance
-(use-package naysayer-theme
+(use-package nord-theme
   :ensure t
   :config
-  (load-theme 'naysayer t))
+  (load-theme 'nord t))
 (set-face-attribute 'default nil
 		    :family "Iosevka Term"
 		    :height 140)
 
 ;; Editing
-(setq make-backup-files nil
-      display-line-numbers-type 'relative)
+(setq confirm-kill-emacs #'y-or-n-p
+      display-line-numbers-type 'relative
+      make-backup-files nil
+      undo-limit (* 20 1024 1024)
+      undo-strong-limit (* 40 1024 1024))
 (setq-default indent-tabs-mode nil
               tab-width 4)
 
@@ -28,13 +31,25 @@
 (fido-vertical-mode 1)
 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(keymap-global-set "C-," #'duplicate-dwim)
+
+;; File management
+(setq delete-by-moving-to-trash t
+      dired-dwim-target t
+      dired-listing-switches "-alh"
+      dired-mouse-drag-files t)
 
 ;; Development
+(require 'ansi-color)
+
+(setq compilation-scroll-output 'first-error)
+(add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
 (use-package rust-mode :ensure t)
 (use-package magit :ensure t)
-(use-package majutsu
-  :vc (:url "https://github.com/0WD0/majutsu")
-  :commands (majutsu majutsu-log))
 
 ;; Restore garbage collection after startup.
 (setq gc-cons-threshold (* 32 1024 1024))
