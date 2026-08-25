72d5cf7bbea6a054ba5a73f95c3df73dfaabccc3
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-08-25 14:24:31 +0530

    emacs: add Dracula theme and editing workflow

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,21 +1,23 @@
 ;;; early-init.el --- Runs before the GUI frame is created -*- lexical-binding: t; -*-
 
+;; Make frame invisible till we load theme and font
+(add-to-list 'initial-frame-alist '(visibility . nil))
+
 ;; Defer garbage collection during startup; init.el restores it.
 (setq gc-cons-threshold most-positive-fixnum)
 
-(setq select-enable-clipboard t)
-
 (setq inhibit-startup-message t
       frame-resize-pixelwise t
-      initial-frame-alist '((fullscreen . maximized))
       default-frame-alist '((tool-bar-lines . 0)
                             (menu-bar-lines . 0)
-                            (vertical-scroll-bars . nil)
-                            (ns-transparent-titlebar . t)
-                            (fullscreen . maximized)))
+                            ;; (vertical-scroll-bars . nil)
+                            (ns-transparent-titlebar . t)))
+                            ;; (fullscreen . maximized)))
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+(setq select-enable-clipboard t)
 
 (setq native-comp-async-report-warnings-errors 'silent)
 
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -9,18 +9,32 @@
 (package-initialize)
 
 ;; Appearance
-(use-package nord-theme
+(setq custom-safe-themes t)
+
+(use-package doom-themes
   :ensure t
+  :custom
+  ;; Global settings (defaults)
+  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
   :config
-  (load-theme 'nord t))
+  (load-theme 'doom-dracula t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 (set-face-attribute 'default nil
 		    :family "Iosevka Term"
-		    :height 140)
+		    :height 150)
+;; After loading theme and font make frame visible again
+(set-frame-parameter nil 'visibility t)
 
-;; Editing
-(setq confirm-kill-emacs #'y-or-n-p
-      display-line-numbers-type 'relative
-      make-backup-files nil
+(setq make-backup-files nil
+      auto-save-default nil
+      desktop-restore-frames nil
+      ;; display-line-numbers-type 'relative
+      ring-bell-function 'ignore
       undo-limit (* 20 1024 1024)
       undo-strong-limit (* 40 1024 1024))
 (setq-default indent-tabs-mode nil
@@ -29,27 +43,45 @@
 (column-number-mode 1)
 (show-paren-mode 1)
 (fido-vertical-mode 1)
+(repeat-mode 1)
+(winner-mode 1)
+(savehist-mode 1)
+(desktop-save-mode 1)
+(save-place-mode 1)
+(global-auto-revert-mode 1)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
-(keymap-global-set "C-," #'duplicate-dwim)
+(keymap-global-set "C-c d" #'duplicate-dwim)
+(keymap-global-set "C-x C-b" #'ibuffer)
+(keymap-global-set "<f5>" #'recompile)
 
-;; File management
 (setq delete-by-moving-to-trash t
       dired-dwim-target t
       dired-listing-switches "-alh"
       dired-mouse-drag-files t)
 
-;; Development
 (require 'ansi-color)
-
-(setq compilation-scroll-output 'first-error)
+(setq compilation-scroll-output 'first-error
+      compilation-always-kill t)
 (add-hook 'compilation-filter-hook #'ansi-color-compilation-filter)
 
-(use-package rainbow-delimiters
+(use-package multiple-cursors
   :ensure t
-  :hook (prog-mode . rainbow-delimiters-mode))
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)
+         ("C-\"" . mc/skip-to-next-like-this)
+         ("C-:" . mc/skip-to-previous-like-this)
+         ("C-S-<mouse-1>" . mc/toggle-cursor-on-click)))
+
 (use-package rust-mode :ensure t)
 (use-package magit :ensure t)
+(use-package vc-jj :ensure t)
 
 ;; Restore garbage collection after startup.
 (setq gc-cons-threshold (* 32 1024 1024))
