eb4c45ad8c740a780ac2bf3a26a6c91b8859f7d3
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-09-25 12:10:23 +0530

    Import current Windows Terminal, PowerShell, and VS Code settings from the Windows PC

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,12 @@
 Set-Alias -Name docker -Value podman
 Remove-Item Alias:\ls -ErrorAction SilentlyContinue
+Set-Alias -Name ls -Value eza
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# mise (env vars + tool paths per directory)
+mise activate powershell | Out-String | Invoke-Expression
