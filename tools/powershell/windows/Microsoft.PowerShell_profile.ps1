84e4d1514df988296802c11123c9dd30d7d258c5
SiddharthPant <10671794+SiddharthPant@users.noreply.github.com>
2026-09-26 12:06:06 +0530

    powershell: fix ssh env vars, vscode: sort keys add word wrap col

--- tools/powershell/windows/Microsoft.PowerShell_profile.ps1
+++ tools/powershell/windows/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,13 @@
-Set-Alias -Name docker -Value podman
+# Over SSH, Windows won't follow the user-created symlinks in WinGet\Links
+# ("untrusted mount point"), so put the real package dirs first on PATH
+if ($env:SSH_CONNECTION) {
+    $wingetDirs = Get-ChildItem "$env:LOCALAPPDATA\Microsoft\WinGet\Links" -File |
+        Where-Object LinkType -eq 'SymbolicLink' |
+        ForEach-Object { Split-Path $_.Target } |
+        Select-Object -Unique
+    $env:PATH = ($wingetDirs -join ';') + ';' + $env:PATH
+}
+
 Remove-Item Alias:\ls -ErrorAction SilentlyContinue
 Set-Alias -Name ls -Value eza
 
